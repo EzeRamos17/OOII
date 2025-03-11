702a57250aabab0108096168df82a911361cbf55
EzeRamos17 <hernan_ram99@hotmail.com>
2025-03-11 20:05:32 -0300

    cambios

--- src/MainAnemico.java
+++ src/MainAnemico.java
@@ -1,9 +1,11 @@
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class MainAnemico {
 	public static void main(String[] args) {
 		var tiempo = new TiempoAnemico();
 		
-		System.out.println("Fecha en formato largo: " + tiempo.getFechaFormatoLargo());
-		System.out.println("Fecha en formato corto: " + tiempo.getFechaFormatoCorto());
+		System.out.println("Fecha en formato largo: " + tiempo.getFecha().format(DateTimeFormatter.ofPattern("EEEE d 'de' MMMM 'de' yyyy", new Locale("es", "ES"))));
+		System.out.println("Fecha en formato corto: " + tiempo.getFecha().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 	}
 }
--- src/TiempoAnemico.java
+++ src/TiempoAnemico.java
@@ -1,23 +1,13 @@
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 public class TiempoAnemico {
 	private LocalDate fecha;
-	
+
 	public TiempoAnemico() {
 		this.fecha = LocalDate.now();
 	}
-	
+
 	public LocalDate getFecha() {
 		return fecha;
-	}
-	
-	public String getFechaFormatoLargo() {
-		return fecha.format(DateTimeFormatter.ofPattern("EEEE d 'de' MMMM 'de' yyyy", new Locale("es", "ES")));
-	}
-	
-	public String getFechaFormatoCorto() {
-		return fecha.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 	}
 }
