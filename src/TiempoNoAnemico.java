3ea089be8485273b2286b884f4cdd6d538d3f7d5
EzeRamos17 <hernan_ram99@hotmail.com>
2025-03-11 15:20:12 -0300

    Update TiempoNoAnemico.java

--- src/TiempoNoAnemico.java
+++ src/TiempoNoAnemico.java
@@ -4,16 +4,16 @@ import java.util.Locale;
 
 public class TiempoNoAnemico {
 	private LocalDate fecha;
-	
+
 	public TiempoNoAnemico() {
 		this.fecha = LocalDate.now();
 	}
-	
+
 	public void imprimirFechaFormatoLargo() {
 		String formatoLargo = fecha.format(DateTimeFormatter.ofPattern("EEEE d 'de' MMMM 'de' yyyy", new Locale("es", "ES")));
 		System.out.println("Fecha en formato largo: " + formatoLargo);
 	}
-	
+
 	public void imprimirFechaFormatoCorto() {
 		String formatoCorto = fecha.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		System.out.println("Fecha en formato corto: " + formatoCorto);
