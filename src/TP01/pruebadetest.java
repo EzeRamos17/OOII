970b2e3ca9cf3fdfb75becde6307cfa3e422c834
EzeRamos17 <hernan_ram99@hotmail.com>
2025-03-13 13:54:38 -0300

    tp 01 1 1

---
+++ src/TP01/pruebadetest.java
@@ -0,0 +1,14 @@
+package TP01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class pruebadetest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
