be600b66647ee2e7a711297195a347bcb4547738
EzeRamos17 <hernan_ram99@hotmail.com>
2025-03-30 16:02:13 -0300

    prueba

--- src/TP01punto1/Test/TestConcurso.java
+++ src/TP01punto1/Test/TestConcurso.java
@@ -32,7 +32,7 @@ class TestConcurso {
 		
 		//participante.inscribirseEn(concurso, hoy.plusDays(2)); //no pasa el test
 		participante.inscribirseEn(concurso, hoy); //pasa el test
-		
+		//prueba
 		//assertTrue(participante.estaInscriptoEn(concurso));//revisar los 10 puntos
 		assertTrue(participante.tienePuntos(10), "El participante debería recibir 10 puntos por inscribirse el primer día.");
 	}
